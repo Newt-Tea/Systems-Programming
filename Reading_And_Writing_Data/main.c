2b82a767ba1f939951ba6abd47e54a539593c82e
Hudson H <Hudsonhargrove01@gmail.com>
2024-09-04 23:03:04 -0500

    added write functionality need to fix negative numbers

--- Reading_And_Writing_Data/main.c
+++ Reading_And_Writing_Data/main.c
@@ -20,110 +20,119 @@ int checkError (int val, const char *msg)
 
 
 int main(){
-    int fd;
-    char buf[3600];
-    int valRd = 0;
+    const signed char bufferSize = 20;
     
-    fd = checkError(open("raw.dat",O_RDONLY),"Failed to open");
+    int fdR = checkError(open("raw.dat",O_RDONLY, 0644),"Failed to open");
+    int fdW = checkError(open("data.dat", O_WRONLY | O_CREAT | O_TRUNC, 0644),"Failed to create/write to file");
 
-    valRd = checkError(read(fd,buf,1000*sizeof(int)),"Failed to read");
-    printf("%s","Read ");
-    printf("%i ", valRd);
-    printf("%s", "bytes, processing ");
-    printf("%i ", valRd/20);
-    printf("%s", "packets.\n");
-// write new values to new file
-// to: grader - I'm not sure how to do this without a nested loop if there's a good implementation could we go over that in class possibly
+    signed char buffer[bufferSize];
+
     int pos = 0;
-    char low;
-    char high;
+    signed short low;
+    signed short high;
     double postShift;
     double postDiv;
 
-    double accel;
+    double accell;
     double angular;
     double angle;
 
-    for (int i = 0 ; i < valRd/20; i++){
-        printf("%s", "Packet ");
-        printf("%i", i+1);
-        printf("%s", ":\n");
+    double *accellPTR = malloc(sizeof(double));
+    double *angularPTR = malloc(sizeof(double));
+    double *anglePTR = malloc(sizeof(double));
+    while(0 != checkError(read(fdR,buffer,bufferSize),"Failed to read")){
 
-        for(int k = 0; k < 20; k+=2){
+        // write new values to new file
+        for(int k = 2; k < 20; k+=2){
             //total iteration value
 
-            if(i == 0){
-                pos = k;
-            }
-            else if (i == 1){
-                pos = k+20;
-            }
-            else{pos = i * k;}
+            pos = k;
 
-            low = buf[pos];
-            high = buf[pos + 1];
-            postShift = ((short)high << 8)|low;
+            low = buffer[pos];
+            high = buffer[pos + 1];
+            postShift = (high << 8)|low;
             postDiv = postShift/32768.0;
-
+            accell = postDiv*16*9.8;
+            angular = postDiv*2000;
+            angle = postDiv*180;  
 
             if(k==2){
-                double accell = postDiv*16*9.8;
+                
                 printf("%s","ax: ");
                 printf("%f", accell);
                 printf("%s","\n");
+
+                write(fdW,*accellPTR);
             }
 
             if(k==4){
                 printf("%s","ay: ");
-
+                printf("%f", accell);
                 printf("%s","\n");
+
+                write(fdW,*accellPTR);
             }
 
             if(k==6){
                 printf("%s","az: ");
-
+                printf("%f", accell);
                 printf("%s","\n");
+
+                write(fdW,*accellPTR);
             }
 
             if(k==8){
                 printf("%s","wx: ");
-
+                printf("%f", angular);
                 printf("%s","\n");
+
+                write(fdW,*angularPTR);
             }
 
             if(k==10){
                 printf("%s","wy: ");
-            
+                printf("%f", angular);
                 printf("%s","\n");
+
+                write(fdW,*angularPTR);
             }
 
             if(k==12){
                 printf("%s","wz: ");
-
+                printf("%f", angular);
                 printf("%s","\n");
+
+                write(fdW,*angularPTR);
             }
 
             if(k==14){
                 printf("%s","Roll: ");
-
+                printf("%f", angle);
                 printf("%s","\n");
+
+                write(fdW,*anglePTR);
             }
 
             if(k==16){
-                printf("%s","Pitch");
-
+                printf("%s","Pitch: ");
+                printf("%f", angle);
                 printf("%s","\n");
+
+                write(fdW,*anglePTR);
             }
 
             if(k==18){
-                printf("%s","Yaw");
-
+                printf("%s","Yaw: ");
+                printf("%f", angle);
                 printf("%s","\n");
+
+                write(fdW,*anglePTR);
             }
         }
     }
 
-    close(fd);
+    close(fdR);
+    close(fdW);
 
     
     return 0;
