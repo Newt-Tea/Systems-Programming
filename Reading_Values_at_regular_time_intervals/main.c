1eaf44498a521e3295fe490a1594218cb48cc770
Hudson H <Hudsonhargrove01@gmail.com>
2024-09-18 20:45:48 -0500

    Completed Nanosleep Exercise

--- Reading_Values_at_regular_time_intervals/main.c
+++ Reading_Values_at_regular_time_intervals/main.c
@@ -3,7 +3,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
-#include<string.h>
+#include <string.h>
+#include <time.h>
 
 
 
@@ -24,42 +25,48 @@ int main(){
     const unsigned int bufferSize = 3*sizeof(double);
     //open files for reading
     
-    int fdAngl = checkError(open("/home/hhargrove/Documents/GitHub/Systems-Programming/Reading_and_Writing_Data2/values/angl.dat", O_RDONLY, 0644),"Failed to read file");
-    int fdRota = checkError(open("/home/hhargrove/Documents/GitHub/Systems-Programming/Reading_and_Writing_Data2/values/rota.dat", O_RDONLY, 0644),"Failed to read file");
-    int fdAccl = checkError(open("/home/hhargrove/Documents/GitHub/Systems-Programming/Reading_and_Writing_Data2/values/accl.dat", O_RDONLY, 0644),"Failed to read file");
+    int fdAngl = checkError(open("/home/hhargrove/Documents/GitHub/Systems-Programming/Reading_and_Writing_Data2/values/angl.dat", O_RDONLY),"Failed to read file");
 
     //An array to store the three double
     double buffer[3];
 
-    int pos = 0;
     double yaw = 0.0;
     double pitch = 0.0;
     double roll = 0.0;
     int setCount = 1;
+
+    const struct timespec req  = {1,0};
+    struct timespec rem = {NULL, NULL};
 
 
     while(0 != checkError(read(fdAngl,buffer,bufferSize),"Failed to read")){
 
         // print the values of angl and wait a second after each information set
 
-            pos = 0;
-            roll = buffer[pos];
-            pitch = buffer [pos + 1];
-            yaw = buffer[pos + 2];
+        roll = buffer[0];
+        pitch = buffer [1];
+        yaw = buffer[2];
 
-            printf("%s","Data Set: ");
-            printf("%i", setCount);
+        printf("%s","Data Set: ");
+        printf("%i", setCount);
 
-            if((roll >= -20.0) && (roll <= 20.0))
-                printf("\n%s","The Roll value is in range\n");
-            else{ printf("%s", "The Roll value is not in range\n");}
+        if((roll >= -20.0) && (roll <= 20.0))
+            printf("\n%s","The Roll value is in range\n");
+        else{ printf("\n%s", "The Roll value is not in range\n");}
 
+        if((pitch >= -20.0) && (pitch <= 20.0))
+            printf("%s","The Pitch value is in range\n");
+        else{ printf("%s", "The Pitch value is not in range\n");}
+
+        if((yaw >= -20.0) && (yaw <= 20.0))
+            printf("%s","The Yaw value is in range\n\n");
+        else{ printf("%s", "The Yaw value is not in range\n\n");}
+
+        nanosleep(&req,&rem);
 
         setCount += 1;
     }
-    close(fdAccl);
     close(fdAngl);
-    close(fdRota);
 
     
     return 0;
